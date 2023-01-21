2e969c6799b20105a91d189ee31f7554e23d2dd6
jason-sims <jason_sims84@outlook.com>
2023-01-21 17:05:34 +0000

    updated content of about and role on main page

--- components/About.jsx
+++ components/About.jsx
@@ -12,27 +12,33 @@ const About = () => {
         <div>
           <h1 className="uppercase pb-8">About Jason</h1>
           <p className="font-medium text-base text-justify leading-relaxed pt-2">
-            I am passionate about understanding user problems and grateful that
-            there is a profession where I can both understand and solve them.
+            Hi, my name is Jason Stuart Sims. I am a Product Manager and
+            aspiring Web Developer.
           </p>
           <p className="font-medium text-base text-justify leading-relaxed pt-2">
-            I grew up in a military family completing high school in Australia
-            in 2001. Originally passing on university, my first working decade
+            Growing up in a military family I completed high school in
+            Australia. Originally passing on university, my first working decade
             was filled with roles in childcare, manufacturing, IT support,
-            training and payroll. In 2013 I completed a Bachelor of Business
-            (Management & Leadership) and secured my first product owner role. I
-            currently live in London, UK.
+            training and payroll. I went back to university to study a Bachelor
+            of Business (Management & Leadership) whilst working full time and
+            upon completion secured my first product role.
           </p>
           <p className="font-medium text-base text-justify leading-relaxed pt-2">
-            I have broad experience across the product life cycle developing
-            products and services in healthcare, government, publishing,
-            insurance and consulting. I am forever curious, I want to understand
-            the entire customer journey and make timely, evidence based
-            decisions.
+            I have gained broad experience across the product life cycle
+            developing products and services in healthcare, government,
+            publishing, insurance and consulting.
           </p>
           <p className="font-medium text-base text-justify leading-relaxed pt-2">
-            In my spare time you will find me developing, renovating, spending
-            time with my family and dreaming about a sailing holiday.
+            I currently live in London, UK with my partner and son.
+          </p>
+          <p className="font-bold text-base text-justify leading-relaxed pt-2">
+            Forever curious I want to understand the customer journey, identify
+            opportunities and make timely, evidence based decisions that create
+            successful products.
+          </p>
+          <p className="font-medium text-base text-justify leading-relaxed pt-2">
+            In my spare time you will find me coding, renovating, spending time
+            with my family and dreaming about a sailing holiday.
           </p>
         </div>
       </div>
